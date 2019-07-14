d2e2e0fdc61e412b13949242a3c04802b093d414
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2019-07-13 22:49:33 -0400

    Update BillOfSale.sol
    elaborate modifiers

--- BillOfSale.sol
+++ BillOfSale.sol
@@ -4,23 +4,33 @@ contract BillOfSale {
 	address public seller;
 	address public buyer;
 	string public descr;
-	uint public price;
+	uint256 public price;
+	
+	event ReceiptConfirmed(address indexed this, address indexed seller);
     
-	function recordContract(string _descr, uint _price,
-    	address _seller, address _buyer) public {
-    	descr = _descr;
-    	price = _price;
-    	seller = _seller;
-    	buyer = _buyer;
-	}
-    
-	function confirmPurchase() public payable {
-    	require(msg.sender == buyer, "only buyer can fund price");
-    	require(price == msg.value);
-	}
-    
-	function confirmReceipt() public {
-    	require(msg.sender == buyer, "only buyer can confirm");
-    	seller.transfer(address(this).balance);
-	}
+	constructor(
+	    string memory _descr, 
+	    uint256 _price,
+    	address _seller, 
+    	address _buyer) 
+    	    public {
+    	            descr = _descr;
+            	    price = _price;
+            	    seller = _seller;
+            	    buyer = _buyer;
+	                 }
+                     /**
+                      * @dev Throws if called by any account other than the buer.
+                      */
+                        modifier onlyBuyer() {
+                        require(msg.sender == buyer);
+                         _;
+                           }
+	                        function confirmPurchase() public payable onlyBuyer {
+    	                    require(price == msg.value);
+	                        }
+	                         function confirmReceipt() public onlyBuyer {
+	                         seller.transfer(address(this).balance);
+	                         emit ReceiptConfirmed(address(this), seller);
+	                         }
 }
